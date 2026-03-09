31e16942d7244c272bb7ad9ec3b728c8417e916c
elMestre <carmesce@gmail.com>
2026-03-09 20:13:51 +0100

    URLS: template URLs and Namespacing

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -13,7 +13,7 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
 
     return render(request, "polls/index.html", context)
-
+"""
 # Imprimimos SIN shortcut: lo mismo pero mas farragoso
 def detail(request, question_id):
     pregunta = get_object_or_404(Question, pk=question_id)
@@ -21,6 +21,14 @@ def detail(request, question_id):
     contexto = {"incognita": pregunta}
 
     return HttpResponse(plantilla.render(contexto, request))
+
+# Mucho mas reducido, pero misma funcionalidad
+"""
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
 
 
 def results(request, question_id):
