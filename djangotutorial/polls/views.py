1ee4d1c22c8152503af655ed36ee92c81cd011f8
elMestre <carmesce@gmail.com>
2026-03-09 19:36:25 +0100

    Comparamos render vs no render

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,20 +1,26 @@
 # from django.shortcuts import render <- Esto seria lo normal
 
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 from .models import Question
 
 
+# Imprimimos con shortcut
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
 
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
+# Imprimimos SIN shortcut: lo mismo pero mas farragoso
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    pregunta = get_object_or_404(Question, pk=question_id)
+    plantilla = loader.get_template("polls/detail.html")
+    contexto = {"incognita": pregunta}
+
+    return HttpResponse(plantilla.render(contexto, request))
 
 
 def results(request, question_id):
