fbe0944058c01093cf91c6498fdde529931d63b5
elMestre <carmesce@gmail.com>
2026-03-11 19:29:25 +0100

    generic views

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,9 +3,9 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
-
 
 # Imprimimos con shortcut
 def index(request):
@@ -13,26 +13,24 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
 
     return render(request, "polls/index.html", context)
-"""
-# Imprimimos SIN shortcut: lo mismo pero mas farragoso
-def detail(request, question_id):
-    pregunta = get_object_or_404(Question, pk=question_id)
-    plantilla = loader.get_template("polls/detail.html")
-    contexto = {"incognita": pregunta}
 
-    return HttpResponse(plantilla.render(contexto, request))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-# Mucho mas reducido, pero misma funcionalidad
-"""
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
